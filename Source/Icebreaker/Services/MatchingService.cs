94508d70ff3e63e519eb78726085019493d35cfa
Abdelrady Tantawy <v-abtan@microsoft.com>
2020-11-02 12:01:40 -0800

    Move matching service to services folder

--- Source/Icebreaker/Services/MatchingService.cs
+++ Source/Icebreaker/Services/MatchingService.cs
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------------
-// <copyright file="IcebreakerBot.cs" company="Microsoft">
+// <copyright file="MatchingService.cs" company="Microsoft">
 // Copyright (c) Microsoft. All rights reserved.
 // </copyright>
 //----------------------------------------------------------------------------------------------
