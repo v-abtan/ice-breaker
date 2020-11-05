7ae899ea1a93609d9481c44f8016b28223b8d68e
Abdelrady Tantawy <v-abtan@microsoft.com>
2020-11-04 17:22:26 -0800

    Update welcome cards to use templates

---
+++ Source/Icebreaker/Helpers/AdaptiveCard.cs
@@ -0,0 +1,25 @@
+﻿namespace Icebreaker.Helpers
+{
+    /// <summary>
+    /// Enum members represent the different cards the bot is sending
+    /// </summary>
+    public enum AdaptiveCardName
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        PairUpNotification,
+        /// <summary>
+        /// 
+        /// </summary>
+        UnrecognizedInput,
+        /// <summary>
+        /// 
+        /// </summary>
+        WelcomeNewMember,
+        /// <summary>
+        /// 
+        /// </summary>
+        WelcomeTeam
+    }
+}
---
+++ Source/Icebreaker/Helpers/CardTemplateHelper.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using AdaptiveCards.Templating;
+
+namespace Icebreaker.Helpers
+{
+    /// <summary>
+    /// Utility functions for constructing cards from templates.
+    /// </summary>
+    public class CardTemplateHelper
+    {
+        private const string CardsPath = "~/Helpers/AdaptiveCards/";
+
+        /// <summary>
+        /// This method will create an instance of adaptiveCardTemplate with the cardPath.
+        /// </summary>
+        /// <param name="cardPath">cardPath specifies card schema to create Template.</param>
+        /// <returns>Adaptive card template.</returns>
+        public static AdaptiveCardTemplate GetAdaptiveCardTemplate(string cardPath)
+        {
+            return new AdaptiveCardTemplate(File.ReadAllText(cardPath));
+        }
+
+        /// <summary>
+        /// This method will create an instance of adaptiveCardTemplate from a card enum member.
+        /// </summary>
+        /// <param name="card">Specifies card name to create Template.</param>
+        /// <returns>Adaptive card template.</returns>
+        public static AdaptiveCardTemplate GetAdaptiveCardTemplate(AdaptiveCardName card)
+        {
+            return GetAdaptiveCardTemplate(Path.Combine(CardsPath, $"{card}AdaptiveCard.json"));
+        }
+
+    }
+}
