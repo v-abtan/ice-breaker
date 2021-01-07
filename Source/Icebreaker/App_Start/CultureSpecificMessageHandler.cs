f542972cbf2571065b8768cbef5e4ae880e25df9
Abdelrady Tantawy <v-abtan@microsoft.com>
2021-01-06 17:22:19 -0800

    Add basic changes for default culture settings

---
+++ Source/Icebreaker/App_Start/CultureSpecificMessageHandler.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="CultureSpecificMessageHandler.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Icebreaker
+{
+    using System.Globalization;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure;
+
+    /// <summary>
+    /// Message handler to set culture specific settings
+    /// </summary>
+    public class CultureSpecificMessageHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var cultureName = CloudConfigurationManager.GetSetting("DefaultCulture");
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureName);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureName);
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
